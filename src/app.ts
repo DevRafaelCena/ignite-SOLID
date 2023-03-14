28c5821e5b117e80c319edc9dc234c6b236302df
Rafael Cena <rafael.cena@hotmail.com>
2023-03-14 09:33:39 -0300

    criando o banco e tabelas

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,14 @@
 import fastify from 'fastify'
+import { PrismaClient } from '@prisma/client'
+
+export const prisma = new PrismaClient()
 
 export const app = fastify()
+
+
+prisma.user.create({
+    data: {
+        name: 'Alice',
+        email: 'rafael.cena@hotmail.com'
+    }
+})
